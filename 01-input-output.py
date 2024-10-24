83f6817b7a795ea8fb8ac816f4b702175cf83564
Talat <ta.rasouli60@yahoo.com>
2024-10-24 17:20:46 +0330

    feat:input output example

---
+++ 01-input-output.py
@@ -0,0 +1,13 @@
+# name=input('Enter your name:')
+
+# ------------------------------------
+# name=input("Enter your name:")
+# age=input("Enter your age:")
+# print('your name is',name,"your age is",age,'years old')
+
+#نمی تونیم سن رو بعلاوه یک کنیم چون استرینگ فقط با استرینگ جمع می شه
+# ----------------------------------------
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print('your name is',name,"your age is",age,'years old')
+
