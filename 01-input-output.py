3ae45bacea5a6f82eb48dd68981e60cc5a940603
Talat <ta.rasouli60@yahoo.com>
2024-10-25 11:03:44 +0330

    feat:about dict

--- 01-input-output.py
+++ 01-input-output.py
@@ -10,4 +10,3 @@
 name=input("Enter your name:")
 age=int(input("Enter your age:"))
 print('your name is',name,"your age is",age,'years old')
-
---
+++ 06-list-vs-dict.py
@@ -0,0 +1,23 @@
+import time 
+
+
+lst = [i for i in range(10_000_000)]
+
+dct = {i:i for i in range(10_000_000)}
+
+import time
+lst = [i for i in range(10_000_000)]
+
+dct = {i: i for i in range(10_000_000)}
+
+start = time.perf_counter()
+print(9_999_999 in lst)
+end = time.perf_counter()
+
+print('list:', end - start)
+
+start = time.perf_counter()
+print(9_999_999 in dct)
+end = time.perf_counter()
+
+print('dict:', end - start)
